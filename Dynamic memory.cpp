dd2f5dd9ddf35d59807f16609fafb4f0e73872a5
Evilnood <evilnood666@gmail.com>
2021-03-16 14:58:04 +0200

    Добавьте файлы проекта.

---
+++ Dynamic memory.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    //creating dynamic array
+
+    int n, m;
+    int* pointer;
+    std::cout << "How big array you want? ";
+    std::cin >> n;
+    pointer = new int[n]; //line that creates dynamic array
+    for (m = 0; m < n; m++)
+    {
+        std::cout << "Enter number here: " << std::endl;
+        std::cin >> pointer[m];
+    }
+    std::cout << "Your numbers: " << std::endl;
+    for (m = 0; m < n; m++)
+    {
+        std::cout << pointer[m]<<", ";
+    }
+    delete[] pointer; //clearing memory
+
+}
